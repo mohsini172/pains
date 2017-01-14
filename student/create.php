2ffb9b0ad1021b8806daa11aff9adda930546550
Affan Muddassir <Affan Muddassir>
2017-01-14 13:54:11 +0500

    new changes pushed

--- student/create.php
+++ student/create.php
@@ -1,1 +1,23 @@
+<?php
+    
 require_once('../db/database.php');
+
+if(isset($_POST['name']) && isset($_POST['id']) && isset($_POST['father_name']) && isset($_POST['cnic']) && isset($_POST['phone']) &&                  isset($_POST['cid']))
+{
+    $name = $_POST['name'];
+    $id = $_POST['id'];
+    $father_name = $_POST['father_name'];
+    $cnic = $_POST['cnic'];
+    $phone = $_POST['phone'];
+    $cid = $_POST['cid'];
+    
+    $db->store("insert into student(`sname`, `father_name`, `scnic`, `phone`, `cid`) values('$name', '$father_name', '$cnic'
+                '$phone', '$cid')");
+}
+
+else
+{
+    die("Invalid arguments");
+}
+
+?>
